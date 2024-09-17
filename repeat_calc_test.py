f278c9ee580c54756eeca3fdb24e2c15b4b33f03
taylor <kaser.taylor@gmail.com>
2024-09-17 16:47:26 -0700

    retries the print rows algorithm with a repeat counter

--- repeat_calc_test.py
+++ repeat_calc_test.py
@@ -6,31 +6,44 @@ def calculate_repeat(rows, rows_between):
 
     return num_repeats
 
-def calculate_remaining_rows(rows, rows_between):
-    remaining_rows = rows % (rows // rows_between)
+def calculate_remaining_rows(rows, rows_between, repeat):
+    remaining_rows = rows - (rows_between * (repeat))
     
     return remaining_rows
     
-def print_regular_repeat(row_counter, regular_repeat):
-    print(f'Row {row_counter} - {row_counter + regular_repeat} sc', '/n')
+def print_regular_repeat(row_counter, rows_between):
+    print(f'Row {row_counter} - {row_counter + rows_between} sc')
 
 def print_increase(row_counter):
     print(f'Row {row_counter} 2sc in first st 2sc in last')
 
-def print_rows(rows, rows_between, remaing_rows):
-    repeat = rows // rows_between
-    row_counter = 2
-    while row_counter < rows - remaing_rows:
-        if row_counter % repeat == 0:
+def print_rows(rows, rows_between, remaining_rows):
+    row_counter = 1
+    repeat_counter = 0
+    while row_counter < rows - remaining_rows:
+        if repeat_counter % rows_between == 0:
             print_increase(row_counter)
             row_counter += 1
-        elif rows - row_counter == remaing_rows:
-            print(f'Sc {row_counter} - {rows}')
-        else:  
-            print_regular_repeat(row_counter, repeat)
+        else:
+            print_regular_repeat(row_counter, rows_between)
             row_counter += rows_between
+            repeat_counter += rows_between
+    else:
+        print(f'Row {row_counter} - {rows} sc')
 
-print_rows(rows, rows_between, calculate_remaining_rows(rows, rows_between))
+print_rows(rows, rows_between, calculate_remaining_rows(rows, rows_between, calculate_repeat(rows, rows_between)))
+
+# def print_rows(rows, rows_between, remaing_rows, repeat):
+#     row_counter = 1
+#     while row_counter < rows:
+#         if row_counter % repeat == 0:
+#             print_increase(row_counter)
+#             row_counter += 1
+#         elif row_counter != rows - remaing_rows:
+#             print_regular_repeat(row_counter, repeat)
+#             row_counter += rows_between
+#         else:
+#             print(f'Sc {row_counter} - {rows}')
 
 # print_rows(rows, calculate_repeat(rows, rows_between), calculate_remaining_rows(rows, rows_between))
 
