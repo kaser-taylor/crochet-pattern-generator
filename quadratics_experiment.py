53eeac332267ab41f7462f40397ea634a7c3bea6
taylor <kaser.taylor@gmail.com>
2025-01-07 13:52:46 -0800

    works on bodice quadratics

--- quadratics_experiment.py
+++ quadratics_experiment.py
@@ -1,14 +1,26 @@
-def find_c(length, stitch_height, slope):
-    x = length // stitch_height
-    c = -slope * (x ** 2)
+def find_c_inc(m1, m2):
+    c = (m1 - m2) / 2
 
     return c
 
-def find_c(m1, m2,  i_d):
-    c = 0
-    if i_d == True:
-        c = (m2 - m1) / 2
-    else:
-        c = (m1 - m2) / 2
-    
+def find_c_dec(m1, m2):
+    c = (m2 - m1) / 2
+
     return c
+
+def find_a_inc(c, length):
+    a = (-c / (-(length ** 2)))
+
+    return a
+
+def find_a_dec(c, length):
+    a = (-c / (length ** 2))
+
+    return a
+
+def find_width_inc(a, length, i, stitch_height, c):
+    length = (length) + (i * stitch_height)
+    y = a * (-(length ** 2)) + c
+
+    return y
+
