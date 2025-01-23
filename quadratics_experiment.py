e3ecc92b4e1912e20115f686635369c1833164f7
Taylor Kaser <kasertaylor@gmail.com>
2025-01-23 14:36:49 -0800

    try hards be trying hard

--- quadratics_experiment.py
+++ quadratics_experiment.py
@@ -1,42 +1,30 @@
-#THIS IS ALL GARBAGE HAVE TO REWRITE ALGO BECAUSE THIS CHANGES THE C VALUE FOR EVERY PASS THROUGH PLEASE LOOK AT DESMOS WHEN YOU WRITE STUFF SO YOU DONT WAIST A MILLION HOURS
-def find_c_inc(m1, m2):
-    c = (m2 - m1) / 2
+# this equation finds the y value for x-intercepts. this is used to find the y values that are plugged into find_a when x = 0 THIS IS ALSO A Y VALUE PLUGGED INTO A AND THE C VALUE PLUGGED INTO FIND Y
+def find_c_y(small_measure, big_measure):
+    y = big_measure - small_measure
+    return y
 
-    return c
-def find_c_dec(m1, m2):
-    c = (m1 - m2) / 2
-
-    return c
-
-# ^^ a problem with the above assumptions is the math is in a 2d space and not a 3d one. I believe most yarns will be flexible enough to accomadate this issue but if enough people bring it up as an issue then we can add some more to the ease
-
-
-# this is where the signs of the functions get weird. So if we remember from the find_c_inc it should return a -c value. a is the slope of the parabola and on an increase in stitches we actually want a  
-def find_a_inc(c, length):
-    a = (-c / (length ** 2))
+#this equation finds the slope of the parabola given two sets of x,y coordinates
+def find_a(y1, y2, x1, x2):
+    a = (y2 - y1) / ((x2 ** 2) - (x1 **2))
 
     return a
 
-def find_a_dec(c, length):
-    a = (-c / (-(length ** 2)))
-
-    return a
-
-def find_y_inc(a, length, i, stitch_height, c):
-    length = length + (i * stitch_height)
-    y = (a * (-(length ** 2))) + c
+#this finds the y value on the slope this is then used to find the width at the certain place in the sweater
+def find_y (a, x, c):
+    y = (a(x ** 2)) + c
 
     return y
 
-def find_y_dec(a, length, i, stitch_height, c):
-    length = 0 + (i * stitch_height)
-    y = (a * (length ** 2)) + c
+#finds the width given a y value
+def find_width(big_measure, y):
+    width = big_measure - (y * 2)
 
-    return y
+    return width
 
-def find_rows(length, stitch_height):
-    rows = length / stitch_height
+def find_stitch_num (width, stich_width):
+    stitch = width // stich_width
 
-    return rows
+    return stitch
+
 
 
